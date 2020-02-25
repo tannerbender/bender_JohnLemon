1ac8cb418152b4253455a02629cdd2328e9ee6ee
tannerbender <54556211+tannerbender@users.noreply.github.com>
2020-02-25 15:27:42 -0500

    update gameEnding script

--- Bender_JohnLemon/Assets/Scripts/GameEnding.cs
+++ Bender_JohnLemon/Assets/Scripts/GameEnding.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
     public GameObject player;
-    public CanvasGroup
-        exitBackgroundImageCanvasGroup;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
 
     void OnTriggerEnter(Collider other)
@@ -18,28 +21,41 @@ public class GameEnding : MonoBehaviour
         if (other.gameObject == player)
         {
             m_IsPlayerAtExit = true;
-
         }
     }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
     void Update()
     {
-        if(m_IsPlayerAtExit)
+        if (m_IsPlayerAtExit)
         {
-            EndLevel();
-
+            EndLevel(exitBackgroundImageCanvasGroup, false);
         }
-        
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+        }
     }
-    void EndLevel()
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-        exitBackgroundImageCanvasGroup.alpha =
-            m_Timer / fadeDuration;
-
-        if(m_Timer > fadeDuration + displayImageDuration)
+        if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
